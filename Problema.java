2da90df7e0d59b9f38638d25c90fe7f061a11fb3
tisiarias04 <tisiano.arias@est.fi.uncoma.edu.ar>
2026-09-01 19:10:57 -0300

    esto es una prueba

---
+++ Problema.java
@@ -0,0 +1,18 @@
+class Problema {
+
+    private String descripcion;
+    private int dificultad;
+
+    public Problema(String descripcion, int dificultad) {
+        this.descripcion = descripcion;
+        this.dificultad = dificultad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getDificultad() {
+        return dificultad;
+    }
+}
